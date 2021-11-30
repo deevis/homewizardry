d7abf4070ee6995ac56bacb3c8ec23214f2f930b
Darren Hicks <darren.hicks@gmail.com>
2021-11-30 01:24:06 -0700

    Root path for application

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
       get :get_message
     end
   end
+
+  root 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
