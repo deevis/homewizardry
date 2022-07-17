36097004efba6dd850e18be21656672f394da31a
Darren Hicks <darren.hicks@gmail.com>
2022-07-16 20:04:09 -0600

    Timezones matter in dockertown

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module SmartRooms
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Mountain Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
